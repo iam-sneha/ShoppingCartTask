e65d9c197bd67c4660125106c6f3b72d269a4fb3
snehas_232 <snehaasg98@gmail.com>
2025-03-28 20:53:40 +0530

    first commit

--- shopping_cart_task/src/app/app-routing.module.ts
+++ shopping_cart_task/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'App', pathMatch: 'full' },
+  { path: 'App', component: AppComponent },
   { path: 'cart', component: CartComponent },
 ];
 
